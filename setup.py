a125d6b4d0c2060752a9363e5bae7d2e82fb5d2c
Petr Dlouhý <petr.dlouhy@email.cz>
2022-01-15 10:13:49 +0100

    bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os
 from setuptools import Command, find_packages, setup
 from setuptools.command.sdist import sdist as original_sdist
 
-version = "1.0.2"
+version = "1.0.3"
 
 
 class compile_messages(Command):
